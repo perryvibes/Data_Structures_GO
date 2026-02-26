25c6d5c3c870c0fb8b58adb2203fcd56d0465bda
perryvibes <iulian.perijoc1111@gmail.com>
2026-02-27 01:02:18 +0200

    update

--- First_Class.go
+++ First_Class.go
@@ -10,16 +10,6 @@ type Clothing struct {
 	size  rune
 }
 
-func Initiate(_sku int, _stock int, _name string, _price float32, _size rune) Clothing {
-	var c Clothing
-	c.sku = _sku
-	c.stock = _stock
-	c.name = _name
-	c.price = _price
-	c.size = _size
-	return c
-}
-
 func ModifyAttribute(c *Clothing) {
 	if c.stock < 20 {
 		c.price *= 1.25
@@ -28,7 +18,7 @@ func ModifyAttribute(c *Clothing) {
 
 func main() {
 
-	var jacket Clothing = Initiate(421, 16, "Leather Jacket", 120.4, 'L')
+	jacket := Clothing{421, 16, "Leather Jacket", 120.4, 'L'}
 	fmt.Println(jacket)
 	ModifyAttribute(&jacket)
 	fmt.Println(jacket)
