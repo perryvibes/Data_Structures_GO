8de64476e6f7e96d2cdc48a8890637e4fc5fc1d2
perryvibes <iulian.perijoc1111@gmail.com>
2026-02-28 21:24:44 +0200

    update: Changed more to GO syntax.

--- Second_Class.go
+++ Second_Class.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 type Movie struct {
@@ -13,47 +12,40 @@ type Movie struct {
 	minAge rune
 }
 
-func ShowVector(vector []*Movie, nrElements int) {
-	for i := 0; i < nrElements; i++ {
-		fmt.Println(*vector[i])
+func ShowVector(vector []*Movie) {
+	for _, value := range vector {
+		fmt.Println(*value)
 	}
 }
 
-func CopyNElements(m []*Movie, nrElements int, nrCopiedElements int) []*Movie {
-	newVector := make([]*Movie, nrElements)
-	for i := 0; i < nrCopiedElements; i++ {
-		newVector[i] = m[i]
+func CopyNElements(m []*Movie, nrCopiedElements int) []*Movie {
+	if nrCopiedElements > len(m) {
+		nrCopiedElements = len(m)
 	}
+	newVector := make([]*Movie, nrCopiedElements)
+	copy(newVector, m[:nrCopiedElements])
 	return newVector
 }
 
-func CopyOnlySomeElements(vector []*Movie, nrElements int, maximBudget float64, newVector *[]*Movie, dimension *int) {
+func CopyOnlySomeElements(vector []*Movie, maximBudget float64, newVector []*Movie, dimension *int) []*Movie {
 	*dimension = 0
-	for i := 0; i < nrElements; i++ {
-		if vector[i].budget < maximBudget {
+	for _, value := range vector {
+		if value.budget < maximBudget {
 			*dimension++
 		}
 	}
-	*newVector = make([]*Movie, *dimension)
-	var k int = 0
-	for i := range nrElements {
-		if vector[i].budget < maximBudget {
-			(*newVector)[k] = vector[i]
-			k++
-		}
-	}
+	newVector = make([]*Movie, *dimension)
+	copy(newVector, vector[:(*dimension)])
+	return newVector
 }
 
-func getFirstConditionedElement(vector []*Movie, nrElements int, searchedName string) Movie {
-	var s Movie
-	s.id = -1
-	s.name = "n/a"
-	for i := range nrElements {
-		if strings.Compare(vector[i].name, searchedName) == 0 {
-			return *vector[i]
+func getFirstConditionedElement(vector []*Movie, searchedName string) Movie {
+	for _, value := range vector {
+		if value.name == searchedName {
+			return *value
 		}
 	}
-	return s
+	return Movie{id: -1, name: "n/a"}
 }
 
 func main() {
@@ -63,12 +55,11 @@ func main() {
 	fmt.Println("=========================================================================")
 
 	// slices
-	var nrMovies int = 3
-	var movies []*Movie
-
-	movies = append(movies, &f1)
-	movies = append(movies, &Movie{2, 100, "Interstellar", 30.5, 16})
-	movies = append(movies, &Movie{3, 90, "Fight Club", 20, 16})
+	movies := []*Movie{
+		&f1,
+		{2, 100, "Interstellar", 30.5, 16},
+		{3, 90, "Fight Club", 20, 16},
+	}
 
 	// arrays (with strict length)
 	// but you have to modify ShowVector parameter from vector[]*Movie to vector[3]*Movie
@@ -76,25 +67,25 @@ func main() {
 	//movies[0] = &f1
 	//movies[1] = &Movie{2, 100, "Interstellar", 30.5, 16}
 	//movies[2] = &Movie{3, 90, "Fight Club", 20, 16}
-	ShowVector(movies, nrMovies)
+	ShowVector(movies)
 
 	fmt.Println("=========================================================================")
 
-	var copiedMovies int = 2
-	newVector := CopyNElements(movies, nrMovies, copiedMovies)
-	ShowVector(newVector, copiedMovies)
+	copiedMovies := 2
+	newVector := CopyNElements(movies, copiedMovies)
+	ShowVector(newVector)
 
 	fmt.Println("=========================================================================")
 
 	var cheapVector []*Movie
-	var limit float64 = 50
+	var limit float64 = 100
 	var cheapVectorDimension int
 
-	CopyOnlySomeElements(movies, nrMovies, limit, &cheapVector, &cheapVectorDimension)
-	ShowVector(cheapVector, cheapVectorDimension)
+	cheapVector = CopyOnlySomeElements(movies, limit, cheapVector, &cheapVectorDimension)
+	ShowVector(cheapVector)
 
 	fmt.Println("=========================================================================")
 
-	fmt.Println(getFirstConditionedElement(movies, nrMovies, "Interstellar"))
+	fmt.Println(getFirstConditionedElement(movies, "Interstellar"))
 
 }
